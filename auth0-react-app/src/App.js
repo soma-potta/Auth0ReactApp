2111b6022b6a1f1db42ff8d577bac31817360211
soma.potta <soma.potta@coles.com.au>
2021-09-11 16:52:54 +1000

    Persist tokens from callback url to local storage

--- auth0-react-app/src/App.js
+++ auth0-react-app/src/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import Profile from "./Profile";
 import Nav from "./Nav";
 import Auth from "./Auth/Auth";
+import Callback from "./Callback";
 
 class App extends Component {
   constructor(props){
@@ -17,7 +18,12 @@ class App extends Component {
         <Route 
           path="/" 
           exact 
-          render={ props => <Home auth={this.auth} {...props} />}/>
+          render={ props => <Home auth={this.auth} {...props} />}
+        />
+        <Route 
+          path="/callback" 
+          render={ props => <Callback auth={this.auth} {...props} />}
+        />
         <Route path="/profile" component={Profile}/>
       </>
     );
